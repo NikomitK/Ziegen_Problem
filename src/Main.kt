331aeafe4871208e45479b396118deddc51f18fd
Niko <nikobo2003@gmail.com>
2024-03-26 19:28:11 +0100

    Code

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,27 @@
 fun main() {
-    println("Hello World!")
+    val wiederholungen = 100000
+    var gewonnenBleiben = 0.0
+    var gewonnenWechseln = 0.0
+    repeat(wiederholungen) {
+        if(ziegenproblemBleiben()) gewonnenBleiben++
+        if(ziegenproblemWechseln()) gewonnenWechseln++
+    }
+    println("Gewonnen: ${gewonnenBleiben/wiederholungen} mal beim Bleiben")
+    println("Gewonnen: ${gewonnenWechseln/wiederholungen} mal beim Wechseln")
+}
+
+fun ziegenproblemBleiben(): Boolean{
+    val doors = Array(3) { false }
+    doors[(0..2).random()] = true
+    val choice = (0..2).random()
+    return doors[choice]
+}
+
+fun ziegenproblemWechseln(): Boolean{
+    val doors = Array(3) { false }
+    doors[(0..2).random()] = true
+    val choice = (0..2).random()
+    val open = (0..2).filter { !doors[it] && it != choice }.random()
+    val newChoice = (0..2).filter { it != choice && it != open }.first()
+    return doors[newChoice]
 }
